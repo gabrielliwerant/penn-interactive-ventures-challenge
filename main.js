9e512ed54d0dbd21dda12489080bde55353acd8f
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-11-28 17:06:23 -0500

    Update app to check for and display chuck norris facts

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const fetchData = function() {
-  const url = 'https://sv443.net/jokeapi/category/Programming/?blacklistFlags=nsfw,religious,political';
+const fetch = function() {
+  const url = 'http://api.icndb.com/jokes/random/3';
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.send(null);
+
+  return xhr;
+};
+
+const getResponse = function(xhr) {
+  const parseData = function(data) { return JSON.parse(data).value; };
 
   xhr.onreadystatechange = function() {
     const DONE = 4;
@@ -10,20 +16,51 @@ const fetchData = function() {
 
     if (xhr.readyState === DONE) {
       if (xhr.status === OK) {
-        console.log(xhr.responseText);
+        return parseData(xhr.responseText);
       } else {
         console.log('Error: ' + xhr.status);
       }
     }
   };
+
+  return xhr.onreadystatechange();
 };
 
-const startApp = function() {
-  const introEl = window.document.querySelector('#intro');
+const display = function(data) {
+  var displayEl = window.document.querySelector('#data-container');
+  var table = '<table><thead><tr><th>Id</th><th>Fact</th></thead><tbody>';
 
-  introEl.innerHTML = '<p>Hello World!</p>';
+  if (data.length > 0) {
+    data.forEach(d => {
+      table += '<tr><td>' + d.id + '</td><td>' + d.joke + '</td></tr>'
+    });
 
-  fetchData();
+    table += '</tbody></table>';
+
+    displayEl.innerHTML = table;
+  } else {
+    displayEl.innerHTML = '<p>No results</p>';
+  }
 };
 
-startApp();
+const start = function() {
+  let data = [];
+  const xhr = fetch();
+  const interval = window.setInterval(
+    function() {
+      data = getResponse(xhr);
+
+      if (data) {
+        window.clearInterval(interval);
+        display(data);
+      }
+    },
+    3000,
+    data,
+    xhr
+  );
+
+  display(data);
+};
+
+start();
